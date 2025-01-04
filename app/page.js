3376f8919cfb719ef7951c29f98f3eca2685a965
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2025-01-04 13:51:18 +0700

    idk

--- app/page.js
+++ app/page.js
@@ -13,11 +13,7 @@ export default function Home() {
   useEffect(() => {
     const handleScroll = () => {
       setScrollPosition(window.scrollY);
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -38,14 +34,13 @@ export default function Home() {
 
   return (
     <div className="overflow-x-hidden">
-      {/* Manually added __NEXT div */}
       <div className="__NEXT">
         {/* Navbar */}
         <div className={`navbar fixed top-0 z-50 w-full transition-all duration-300 ${isScrolled ? 'bg-[#0F172A] bg-opacity-90 backdrop-blur-lg border-opacity-30' : 'bg-[#0F172A]'} text-white`}>
           <div className="container mx-auto flex justify-between items-center px-4 lg:px-20">
             <Link href="/" className="btn btn-ghost normal-case text-xl flex items-center space-x-2">
               <span className="text-2xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-yellow-400">&lt;/&gt;</span>
-              <span className="text-xl font-bold text-white space-x-10">Dzikri Maulana</span>
+              <span className="text-xl font-bold text-white">Dzikri Maulana</span>
             </Link>
             <div className="flex-none">
               <ul className="menu menu-horizontal p-0 space-x-8 hidden md:flex">
@@ -88,24 +83,23 @@ export default function Home() {
         )}
 
         {/* Hero Section */}
-        <section className="relative min-h-screen bg-[#0F172A] py-20 overflow-hidden">
-          <div className="container mx-auto px-4 lg:px-20 flex flex-col lg:flex-row items-center justify-between space-y-10 lg:space-y-0">
+        <section className="relative mt-10 min-h-screen bg-[#0F172A] py-10 sm:py-10 overflow-hidden">
+          <div className="container sm:mt-10 mx-auto px-4 sm:px-8 lg:px-20 flex flex-col lg:flex-row items-center justify-between space-y-10 lg:space-y-0">
             <div
-              className={`text-left max-w-2xl lg:max-w-3xl space-y-4 relative z-10 p-4 lg:p-14 bg-black bg-opacity-50 rounded-lg ${hasLoaded ? 'animate-slide-up' : ''}`}
+              className={`text-left max-w-2xl lg:max-w-3xl space-y-4 relative z-10 p-4 sm:p-6 lg:p-14 ${hasLoaded ? 'animate-slide-up' : ''}`}
             >
-              <h1 className="text-4xl font-bold text-white">Hello 👋</h1>
-              <h2 className="text-2xl font-bold text-white">I am,</h2>
-              <h3 className="text-2xl font-extrabold bg-gradient-to-r uppercase from-blue-500 via-purple-500 to-yellow-400 text-transparent bg-clip-text tracking-wider">
+              <h1 className="text-4xl sm:text-4xl font-bold text-white">Hello 👋</h1>
+              <h2 className="text-xl sm:text-2xl font-bold text-white">I am,</h2>
+              <h3 className="text-3xl sm:text-4xl font-extrabold bg-gradient-to-r uppercase from-blue-500 via-purple-500 to-yellow-400 text-transparent bg-clip-text tracking-wider">
                 <div className="flex flex-col space-y-2">
-                  {/* Nama yang muncul satu per satu */}
-                  <span className="text-4xl typing-animation">Muhammad Dzikri Maulana</span>
-                  <span className="text-2xl text-white">Fullstack Developer</span>
+                  <span className="text-4xl">Muhammad Dzikri Maulana</span>
+                  <span className="text-lg text-white">Fullstack Developer</span>
                 </div>
               </h3>
-              <p className="text-lg text-white mt-4">
+              <p className="text-lg text-justify sm:text-lg text-white mt-4 sm:text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae libero ac risus egestas placerat. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               </p>
-              <Link href="#projects" className="btn btn-primary">See More</Link>
+              <Link href="#projects" className="btn btn-primary mt-6">See More</Link>
             </div>
 
             <div className={`relative mt-10 lg:mt-0 z-10 ${hasLoaded ? 'animate-slide-left' : ''}`}>
@@ -125,29 +119,6 @@ export default function Home() {
 
         {/* CSS Animations */}
         <style jsx>{`
-          @keyframes typing {
-            0% {
-              width: 0;
-            }
-            100% {
-              width: 100%;
-            }
-          }
-
-          .typing-animation {
-            display: inline-block;
-            overflow: hidden;
-            white-space: nowrap;
-            border-right: 4px solid rgba(255, 255, 255, 0.75);
-            animation: typing 4s steps(25) 1s forwards, blink 0.75s step-end infinite;
-          }
-
-          @keyframes blink {
-            50% {
-              border-color: transparent;
-            }
-          }
-
           @keyframes slide-up {
             0% {
               transform: translateY(20px);
